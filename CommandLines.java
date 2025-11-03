acfcba857e3be5cf9b1a7c4db41d3f59b9282919
Shivam Parashar <shivamparashar1213@gmail.com>
2025-11-03 18:09:59 +0530

    Update CommandLines.java

--- CommandLines.java
+++ CommandLines.java
@@ -6,14 +6,12 @@ For	                      Commands	                                          Wor
 	                        systeminfo	                                        Shows Your PC's Details	cd.. one folder back
 Java	
 	                        For Run Java Program
-
                           C:\Users\Your Name>javac Main.java
                           C:\Users\Your Name>java Main
                           Hello World
 
 Appium
                           abd.exe devices for Command line
-
                           DesiredCapabilities dc = new DesiredCapabilities();                            Open Appium and Coonect
                           dc.setCapability(MobileCapabilityType.AUTOMATION_NAME, ""Appium"");            For Verify : abd.exe devices
                           dc.setCapability(MobileCapabilityType.PLATEFORM_NAME, ""Android"");            then use Eclipse
@@ -22,7 +20,6 @@ Appium
                           dc.setCapability(MobileCapabilityType.APP, ""F\\Google Drive\\Trainning Material\\Appium\\APK Files\\IndiaMART.apk"");
 
 Commands - SQL + Jenkins + Appium
-
 CMD
 cd /d d:  	for change drive
 cd\  		start from c drive
@@ -39,6 +36,16 @@ __________________________________________
 To satrt server - 			appium
 For app web version - 		appium --allow-cors
 To clean aommand prompt - 	cls
+_________________________________________
+mysql --version
+mysqld --console
+mysql -u root -p
+MYSQL password : Password@1213
+clear cmd - system cls
+C:\Program Files\MySQL\MySQL Server 8.0\bin
+C:\ProgramData\MySQL\MySQL Server 8.0
+
+mysqld --defaults-file="C:\\ProgramData\MySQL\MySQL Server 8.0\\my.ini" --init-file=c:\\mysql-init.txt --console
 
 Common:
 Location file : 	cd bqurious
@@ -52,17 +59,5 @@ i- Launch chrome://flags/#allow-insecure-localhost on Chrome
 ii- Look for the option "Allow invalid certificates for resources loaded from localhost." and enable the option.
 iii- Relaunch Chrome
 iv- Access BQ Platform and try Recording/Local Execution.
-_________________________________________
-mysql --version
-mysqld --console
-mysql -u root -p
-MYSQL password : Password@1213
-clear cmd - system cls
-C:\Program Files\MySQL\MySQL Server 8.0\bin
-C:\ProgramData\MySQL\MySQL Server 8.0
-
-mysqld --defaults-file="C:\\ProgramData\MySQL\MySQL Server 8.0\\my.ini" --init-file=c:\\mysql-init.txt --console
-
-
 
 
